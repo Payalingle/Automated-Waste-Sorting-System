52ee056b23b64fb0de0505b62bf98930db8a80ac
Payalingle <114146182+Payalingle@users.noreply.github.com>
2024-03-17 12:27:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+
+import cvzone
+from cvzone.ClassificationModule import Classifier
+import cv2
+
+cap = cv2.VideoCapture(0)
+classifier = Classifier('Model/keras_model.h5', 'Model/labels.txt')
+imgArrow = cv2.imread('arrow.png', cv2.IMREAD_UNCHANGED)
+classIDBin = 0
+# Import all the waste images
+imgWasteList = []
+pathFolderWaste = "Waste"
+pathList = os.listdir(pathFolderWaste)
+for path in pathList:
+    imgWasteList.append(cv2.imread(os.path.join(pathFolderWaste, path), cv2.IMREAD_UNCHANGED))
+
+# Import all the waste images
+imgBinsList = []
+pathFolderBins = "Bins"
+pathList = os.listdir(pathFolderBins)
+for path in pathList:
+    imgBinsList.append(cv2.imread(os.path.join(pathFolderBins, path), cv2.IMREAD_UNCHANGED))
+
+# 0 = Recyclable
+# 1 = Hazardous
+# 2 = Food
+# 3 = Residual
+
+classDic = {0: None,
+            1: 0,
+            2: 0,
+            3: 1,
+            4: 2,
+            5: 2,
+            6: 1,
+            7: 3}
+
+while True:
+    _, img = cap.read()
+    imgResize = cv2.resize(img, (454, 340))
+
+    imgBackground = cv2.imread('background.png')
+
+    predection = classifier.getPrediction(img)
+
+    classID = predection[1]
+    print(classID)
+    if classID != 0:
+        imgBackground = cvzone.overlayPNG(imgBackground, imgWasteList[classID - 1], (940, 130))
+        imgBackground = cvzone.overlayPNG(imgBackground, imgArrow, (978, 320))
+
+        classIDBin = classDic[classID]
+
+    imgBackground = cvzone.overlayPNG(imgBackground, imgBinsList[classIDBin], (895, 374))
+
+    imgBackground[148:148 + 340, 159:159 + 454] = imgResize
+    # Displays
+    # cv2.imshow("Image", img)
+    cv2.imshow("Output", imgBackground)
+    cv2.waitKey(1)
+    imgBackground = cvzone.overlayPNG(imgBackground, imgBinsList[classIDBin], (895, 374))
+
+    imgBackground[148:148 + 340, 159:159 + 454] = imgResize
+    # Displays
+    cv2.imshow("Image", img)
+    cv2.imshow("Output", imgBackground)
+    cv2.waitKey(1)
